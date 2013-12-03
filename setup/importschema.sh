017d5f90e85b98cfe9fb7124f7eddc9d781a04e6
Wade Wegner <wade.wegner@gmail.com>
2013-12-03 12:54:48 -0700

    Separated demo.md into demo.md and setup.md

--- setup/importschema.sh
+++ setup/importschema.sh
@@ -1,2 +1,13 @@
+echo "Starting the demo schema import process. This should only take a few minutes to complete."
+echo ""
+echo "Step 1: Log in to your Salesforce organization. Close the browser window when complete."
+
 force login
+
+echo "Step 2: Starting the metadata import process using the Force CLI tool."
+echo ""
+
 force import metadata
+
+echo ""
+echo "The import process has completed."
